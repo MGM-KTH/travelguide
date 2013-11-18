ca4e26a04fdee6523abeed65310b1da715b7bed1
glindste <gustaflindstedt@gmail.com>
2013-11-18 16:55:55 +0100

    read input

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
 int main(int argc, char *argv[]) {
-    printf("Hello\n");
+    int nodecount;
+    scanf("%d", &nodecount);
+
+    float x[nodecount];
+    float y[nodecount];
+
+    int i;
+
+    for(i = 0; i < nodecount; i++) {
+        scanf("%f %f", &x[i], &y[i]);
+    }
+
+    print_array(x, nodecount);
+
+    print_array(y, nodecount);
 
     return 0;
+}
+
+void print_array(float array[], int length) {
+    int i;
+    for(i = 0; i < length; i++) {
+        printf("%f\n", array[i]);
+    }
 }
---
+++ main.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+void print_array(float array[], int length);
