928622ae6168f4cc49287eee72ff6bd99dfc5fad
mrunelov <martin.runelov@gmail.com>
2013-11-28 14:51:55 +0100

    Added a header guard

--- main.h
+++ main.h
@@ -1,5 +1,9 @@
+#ifndef TRAVELGUIDE_MAIN_H
+#define TRAVELGUIDE_MAIN_H
 
 void print_farray(float array[], int length);
 void print_iarray(int array[], int length);
 void print_tour(int array[], int length);
 void print_diag_matrix(int matrix[], int nodecount);
+
+#endif
